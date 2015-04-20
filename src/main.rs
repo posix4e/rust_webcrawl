87bfffa9c943802339a804c979b220ae14533d11
Alex HotShot Newman <alex.newman@wandisco.com>
2015-04-20 12:26:34 -0700

    Actually allow the program to finish
    	Move error logging to stderr

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,11 @@ use hyper::Client;
 
 use std::collections::HashSet;
 use hyper::client::response::Response;
-use std::sync::mpsc::channel;
+use std::sync::Arc;
+use std::sync::atomic::{AtomicUsize,Ordering};
+use std::sync::mpsc::{channel,TryRecvError};
+use std::thread;
+use std::io::Write;
 
 fn get_urls_from_html(mut response: Response) -> Vec < String > {
     let mut matched_urls = Vec::new();
@@ -38,13 +42,20 @@ fn get_websites_helper(url_to_crawl: String) -> Vec < String > {
     let mut client = Client::new();
     let res = match client.get(& * url_to_crawl).send() {
         Ok(res) => res,
-        Err(err) => panic!("Failed to connect: {:?}", err)
+        Err(err) => {
+            match writeln!(&mut std::io::stderr(), "Error: {}!", err) {
+                Ok(_) => {},
+                Err(x) => panic!("Unable to write to stderr: {}", x),
+            }
+            return Vec::new();
+        }
     };
     return get_urls_from_html(res);
 }
 
 fn get_websites(url: String) {
     let pool = ThreadPool::new(3000);
+    let running_threads = Arc::new(AtomicUsize::new(0));
     let mut found_urls:HashSet < String > = HashSet::new();
     println!("Crawling {}", url);
     let (tx, rx) = channel();
@@ -52,8 +63,9 @@ fn get_websites(url: String) {
 
     let mut counter = 0;
 
-    while true {
-        match rx.recv() {
+    loop {
+        let n_active_threads = running_threads.compare_and_swap(0, 0, Ordering::SeqCst);
+        match rx.try_recv() {
             Ok(new_site) => {
                 let new_site_copy = new_site.clone();
                 let tx_copy = tx.clone();
@@ -62,17 +74,26 @@ fn get_websites(url: String) {
                 print!("{} ", counter);
                 if !found_urls.contains(&new_site) {
                     found_urls.insert(new_site);
-
+                    running_threads.fetch_add(1, Ordering::SeqCst);
+                    let my_running_threads = running_threads.clone();
                     pool.execute(move || {
                         for new_url in get_websites_helper(new_site_copy) {
                             if new_url.starts_with("http") {
                                 tx_copy.send(new_url).unwrap();
                             }
                         }
+                        my_running_threads.fetch_sub(1, Ordering::SeqCst);
                     });
                 }
             }
-            Err(_) => {}
+            Err(TryRecvError::Empty) if n_active_threads == 0 => break,
+            Err(TryRecvError::Empty) => {
+                writeln!(&mut std::io::stderr(),
+                 "Channel is empty, but there are {} threads running",
+                  n_active_threads);
+        thread::sleep_ms(10);
+            },
+            Err(TryRecvError::Disconnected) => unreachable!(),
         }
     }
 }
